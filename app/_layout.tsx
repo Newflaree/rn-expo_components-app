53ccbfd7ded38df7dce65450933e5a0722341b53
Camilo López <camiloj.lopez79@gmail.com>
2025-06-12 15:08:17 -0400

    ThemedView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,23 @@
-import { Text, View } from 'react-native';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  Text,
+  View
+} from 'react-native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import 'react-native-reanimated';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { ThemedView } from '@/presentation/shared';
 import '../global.css'
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor({}, 'background');
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -18,18 +29,20 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <View className='bg-light-background dark:bg-dark-background'>
-        <Text
-          className='mt-10 text-3xl font-bold text-light-text dark:text-dark-text'
-        >
-          Hola Mundo
-        </Text>
-      </View>
+    <GestureHandlerRootView style={{ backgroundColor: backgroundColor, flex: 1 }}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <ThemedView margin>
+          <Text
+            className='mt-10 text-3xl font-bold text-light-text dark:text-dark-text'
+          >
+            Hola Mundo
+          </Text>
+        </ThemedView>
+      </ThemeProvider>
       {/*
       <Stack>
       </Stack>
         */}
-    </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
