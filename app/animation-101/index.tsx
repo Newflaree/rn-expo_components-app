8ac23dcdc431d76f064969f4432fc8d3250ef428
Camilo López <camiloj.lopez79@gmail.com>
2025-06-12 23:58:14 -0400

    Animaciones 101 - Básicas

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,14 +1,46 @@
-import { View, Text } from 'react-native';
+import { useRef } from 'react';
+import { Animated } from 'react-native';
 import { ThemedButton, ThemedView } from '@/presentation/shared';
 
+
 const Animation101Screen = () => {
+  const animatedOpacity = useRef( new Animated.Value(0) ).current;
+
+  const fadeIn = () => {
+    Animated.timing( animatedOpacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+  }
+
+  const fadeOut = () => {
+    Animated.timing( animatedOpacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+  }
+
   return (
-    <ThemedView margin>
-      <ThemedButton className='mb-2' onPress={ () => console.log( 'fadeIn' ) }>
+    <ThemedView
+      margin
+      className='justify-center items-center flex-1'
+    >
+      <Animated.View
+        className='bg-light-secondary dark:bg-dark-secondary rounded-xl'
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpacity
+        }}
+      />
+
+      <ThemedButton className='my-5' onPress={ fadeIn }>
         FadeIn
       </ThemedButton>
 
-      <ThemedButton className='mb-2' onPress={ () => console.log( 'fadeOut' ) }>
+      <ThemedButton className='my-5' onPress={ fadeOut }>
         FadeOut
       </ThemedButton>
     </ThemedView>
