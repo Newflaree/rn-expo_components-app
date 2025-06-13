d21b58c6d341b247738ae46a4041c219a81467cc
Camilo López <camiloj.lopez79@gmail.com>
2025-06-12 23:05:04 -0400

    Continuación de proyecto

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native';
+import { ThemedButton, ThemedView } from '@/presentation/shared';
 
 const Animation101Screen = () => {
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedButton className='mb-2' onPress={ () => console.log( 'fadeIn' ) }>
+        FadeIn
+      </ThemedButton>
+
+      <ThemedButton className='mb-2' onPress={ () => console.log( 'fadeOut' ) }>
+        FadeOut
+      </ThemedButton>
+    </ThemedView>
   );
 };
 export default Animation101Screen;
