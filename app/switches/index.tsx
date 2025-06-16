0b1217f887039f92e4684931b9604932227f88bc
Camilo López <camiloj.lopez79@gmail.com>
2025-06-15 20:59:54 -0400

    Código por plataforma

--- app/switches/index.tsx
+++ app/switches/index.tsx
@@ -28,12 +28,25 @@ const Switches = () => {
           value={ state.isActive }
         />
           */}
-
         <ThemedSwitch
           text='Activo'
           value={ state.isActive }
           onValueChange={ (value) => setState({ ...state, isActive: value }) }
-          className='mb-2'
+          className='mb-4'
+        />
+
+        <ThemedSwitch
+          text='Hambriento'
+          value={ state.isHungry }
+          onValueChange={ (value) => setState({ ...state, isHungry: value }) }
+          className='mb-4'
+        />
+
+        <ThemedSwitch
+          text='Contento'
+          value={ state.isHappy }
+          onValueChange={ (value) => setState({ ...state, isHappy: value }) }
+          className='mb-4'
         />
       </ThemedCard>
     </ThemedView>
--- presentation/shared/ThemedSwitch.tsx
+++ presentation/shared/ThemedSwitch.tsx
@@ -2,10 +2,12 @@
 import { 
   Pressable,
   View,
+  Platform,
   Text,
   Switch,
 } from 'react-native';
 import { ThemedText } from './';
+import { useThemeColor } from '@/hooks/useThemeColor';
 
 
 interface Props {
@@ -16,30 +18,44 @@ interface Props {
   onValueChange: ( value: boolean ) => void;
 }
 
+const isAndroid = Platform.OS === 'android';
+
 const ThemedSwitch = ({
   text,
   value,
   className,
   onValueChange
 }: Props) => {
+  const switchActiveColor = useThemeColor({}, 'primary');
+
   return (
     <Pressable
       className={`
         flex
         flex-row
         items-center
+        mx-2
         justify-between
         active-opacity-80
         ${ className }
       `}
+      onPress={ () => onValueChange( !value ) }
     >
       {
-        text && (<ThemedText className='h2 mx-2'>{ text }</ThemedText>)
+        text
+          ? <ThemedText className='h2 mx-2'>{ text }</ThemedText>
+          : <View />
       }
 
       <Switch
         value={ value }
         onValueChange={ onValueChange }
+        thumbColor={ isAndroid ? switchActiveColor : '' }
+        //ios_backgroundColor={ value ? 'green' : 'red' }
+        trackColor={{
+          false: 'gray',
+          true: switchActiveColor
+        }}
       />
     </Pressable>
   );
