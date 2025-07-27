3d862188c4ea20e6c4b3824ad3b8e7543dcb7c2d
Camilo López <camiloj.lopez79@gmail.com>
2025-07-27 12:26:54 -0400

    TextInput y ThemedTextInput

--- app/text-inputs/index.tsx
+++ app/text-inputs/index.tsx
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import ThemedCard from '@/presentation/shared/ThemedCard';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedTextInput from '@/presentation/shared/ThemedTextInput';
+import ThemedView from '@/presentation/shared/ThemedView';
 
 const TextInputsScreen = () => {
+  const [ form, setForm ] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
   return (
-    <View>
-      <Text>TextInputsScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedCard className='mb-5'>
+        <ThemedTextInput
+          placeholder='Nombre Completo'
+          autoCapitalize='words'
+          autoCorrect={ true }
+          onChangeText={ (text) => setForm({ ...form, name: text }) }
+        />
+      </ThemedCard>
+
+      <ThemedCard>
+        <ThemedText>
+          { JSON.stringify( form, null, 2 ) }
+        </ThemedText>
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default TextInputsScreen;
