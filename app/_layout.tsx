11ee992ea03cb189fb8bb1edd43f9b4811590943
Camilo López <camiloj.lopez79@gmail.com>
2025-12-09 23:05:42 -0300

    ThemedView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,28 +2,34 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 import '../global.css';
 import {Text, View} from 'react-native';
+import {useThemeColor} from '@/hooks/use-theme-color';
+import {ThemedView} from '@/presentation/shared/ThemedView';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor( {}, 'background' );
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <View className='flex-1 bg-light-background dark:bg-dark-background'>
-        <Text className='mt-10 text-3xl text-light-primary dark:text-dark-primary'> Hola Mundo</Text>
-      </View>
-      { /*
-        <Stack>
-      </Stack>
-       */ }
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ backgroundColor: backgroundColor, flex: 1 }}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <ThemedView margin>
+          <Text className='mt-10 text-3xl text-light-primary dark:text-dark-primary'> Hola Mundo</Text>
+        </ThemedView>
+        { /*
+          <Stack>
+        </Stack>
+         */ }
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
